d93244215ca86f36026c249b42fa9f0c7e396430
MahaGowtham <madhumeena.k1994@gmail.com>
2022-01-05 15:18:42 +0530

    new git

--- SDET/src/main/java/com/crm/vtiger/objectRepository/PBaseClass.java
+++ SDET/src/main/java/com/crm/vtiger/objectRepository/PBaseClass.java
@@ -23,7 +23,7 @@ public class PBaseClass extends WebDriverUtilities{
 		public WebDriverUtilities wu = new WebDriverUtilities();
 		public JavaUtilities ju = new JavaUtilities();
 	
-		@BeforeClass(groups={"SmokeTest","RegressionTest"})
+		@BeforeClass
 		public void launchBrowser() throws Exception
 		{
 			//System.out.println("Launch the Browser");
@@ -38,7 +38,7 @@ public class PBaseClass extends WebDriverUtilities{
 			driver.get(pfile.readDataFromProperties("URL"));
 		}
 		
-		@BeforeMethod(groups={"SmokeTest","RegressionTest"})
+		@BeforeMethod
 		public void createlogin() throws Exception {
 			LoginPage lp = new LoginPage(driver);
 			String user = pfile.readDataFromProperties("username");
@@ -46,14 +46,14 @@ public class PBaseClass extends WebDriverUtilities{
 			lp.loginApp(user, pass);
 		}
 		
-		@AfterMethod(groups={"SmokeTest","RegressionTest"})
+		@AfterMethod
 		public void logoutPage() {
 			LogoutPage lop = new LogoutPage(driver);
 			lop.administrator();
 			lop.logout();
 		}
 		
-		@AfterClass(groups={"SmokeTest","RegressionTest"})
+		@AfterClass
 		public void closeBrowser() {
 driver.close();
 }
